198812f242daa6d32d20eb86a64b393da20983e1
Samandar000 <samandarnfh.2020@gmail.com>
2022-10-04 20:30:08 +0500

    05

--- create_some_objects.py
+++ create_some_objects.py
@@ -1,4 +1,11 @@
 from create_one_attribute import Person
 
+def __init__(self,name):
+        self.name = name
+p1 = Person("Anvar")
+# p1 = p1.name
+p2 = Person("Shavkat")
+# p2 = p2.name
+
 #create an object named "p1" whose name is "Anvar"
 #create an object named "p2" whose name is "Shavkat"
