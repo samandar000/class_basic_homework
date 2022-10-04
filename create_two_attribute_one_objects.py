f0c775c18bc5334346288becf8fd7e781f0d90c4
Samandar000 <samandarnfh.2020@gmail.com>
2022-10-04 23:39:27 +0500

    Final

--- create_two_attribute_one_objects.py
+++ create_two_attribute_one_objects.py
@@ -2,8 +2,7 @@ from create_two_attribute import Person
 def __init__(self,name,age):
     self.name = name
     self.age = age
-person = Person("Ali","25")
-
+person = Person("Ali",25)
 
 
 #create an object named "person" whose name is "Ali", age is "25"
